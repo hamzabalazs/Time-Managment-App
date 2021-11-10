a19fddd5ac6aeab22034b125e3786d46eb8bdd04
hamzabalazs <hamza.z.balazs@student.ms.sapientia.ro>
2021-11-10 11:48:26 +0200

    feat: Working login/register

--- app/src/main/java/com/example/tma/RegisterActivity.java
+++ app/src/main/java/com/example/tma/RegisterActivity.java
@@ -1,23 +1,95 @@
 package com.example.tma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import javax.security.auth.login.LoginException;
 
 public class RegisterActivity extends AppCompatActivity {
-    private Button button;
+    TextView loginbtn;
+    EditText mFullName, mEmail, mPassword;
+    Button registerbtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        button = (Button) findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener(){
+        mFullName = findViewById(R.id.editTextName);
+        mEmail = findViewById(R.id.editTextEmail);
+        mPassword = findViewById(R.id.editTextPass);
+        registerbtn = findViewById(R.id.button);
+        loginbtn = findViewById(R.id.textView4);
+        fAuth = FirebaseAuth.getInstance();
+        progressbar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+
+        registerbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String name = mFullName.getText().toString().trim();
+
+                if(TextUtils.isEmpty(name)){
+                    mFullName.setError("Full Name is Required!");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is Required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is Required!");
+                    return;
+                }
+
+                if(password.length() < 8){
+                    mPassword.setError("Password must be atleast 8 characters long!");
+                    return;
+                }
+
+                progressbar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this,"User Created!",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(RegisterActivity.this,"Error! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 openLogin();
