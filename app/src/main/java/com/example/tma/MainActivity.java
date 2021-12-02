657bbefdd8ba580287dd90345699fa987b0980a4
tothadam98 <toth.adam@student.ms.sapientia.ro>
2021-12-02 11:07:04 +0300

    Added calendar_view.xml to the project and CalendarActivity class

--- app/src/main/java/com/example/tma/MainActivity.java
+++ app/src/main/java/com/example/tma/MainActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button calendarButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),CalendarActivity.class));
+            }
+        });
     }
 
     public void logout(View v){
@@ -21,4 +30,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(),LoginActivity.class));
         finish();
     }
+
+
 }
